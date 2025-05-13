6352fcb66d1e75fa3507cddd72ac1868207bb31a
Thanh Tuyền <tuyen13022003@gmail.com>
2025-05-13 17:07:10 +0700

    create and config project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './index.css';
+
+function App() {
+  return <div className='bg-red-400'>App</div>;
+}
+
+export default App;
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  isLoggedIn: true,
+  token: 123456,
+};
+
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default authReducer;
